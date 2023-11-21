177cc0cad06b51229aaced1cd54fd9ef6a6b9a4e
beaustar2 <beautypop4sure@gmail.com>
2023-11-21 05:08:58 -0700

    commit changes

---
+++ venv/tests/tests_home_route.py
@@ -0,0 +1,6 @@
+from Flask_web.app import app
+
+def test_home_route():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
