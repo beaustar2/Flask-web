5e06cdb9debd664d2ce8e4f2e5f4625ab0ca0614
beaustar2 <beautypop4sure@gmail.com>
2023-11-23 19:59:12 -0700

    update app.py

--- app.py
+++ app.py
@@ -1,25 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('home'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
 @app.route('/projects')
 def projects():
-    return "Explore my exciting projects here!"
+    projects = Project.query.all()
+    return render_template('projects.html', projects=projects)
+
 
 @app.route('/about')
 def about():
-    return "Learn more about me and my DevOps journey."
+    return render_template('about.html')
+
 
 @app.route('/contact')
 def contact():
-    return "Feel free to reach out to me! Phone: +1 (555) 123-4567, Email: info@example.com"
+    return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
 
 
 
