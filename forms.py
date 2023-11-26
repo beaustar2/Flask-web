5bc2b6da34fe3fa9a0bbe6193ecc89a254adfe8f
beaustar2 <beautypop4sure@gmail.com>
2023-11-25 20:24:23 -0700

    commit change

--- forms.py
+++ forms.py
@@ -1,7 +1,6 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields import PasswordField
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
